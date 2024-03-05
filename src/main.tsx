f28fae3d87c626ab31f648e57814bededac94037
Fabio Biondi <info@fabiobiondi.com>
2024-03-05 18:06:41 +0100

    remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 // main.tsx
-import axios from 'axios';
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
 import Cms from './pages/cms/Cms.tsx';
-import Home, { HomePageData } from './pages/home/Home.tsx';
+import Home from './pages/home/Home.tsx';
 import Shop from './pages/shop/Shop.tsx';
 
 
