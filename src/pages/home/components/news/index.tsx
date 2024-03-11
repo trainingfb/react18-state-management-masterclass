b14f6a4f5d0db6e4f19b979de7c150ca982b2820
Fabio Biondi <info@fabiobiondi.com>
2024-03-11 17:25:35 +0100

    remove props spred

--- src/pages/home/components/news/index.tsx
+++ src/pages/home/components/news/index.tsx
@@ -5,8 +5,20 @@ import NewsList from './NewsList.tsx';
 export default function NewsBlock (props: HomePageData & { onSend: (email: string) => void}) {
   return (
     <div>
-      <NewsletterForm {...props} />
-      <NewsList {...props} />
+      <NewsletterForm
+        newsletter={props.newsletter}
+        products={props.products}
+        dealOfTheWeek={props.dealOfTheWeek}
+        news={props.news}
+        onSend={props.onSend}
+      />
+
+      <NewsList
+        newsletter={props.newsletter}
+        products={props.products}
+        dealOfTheWeek={props.dealOfTheWeek}
+        news={props.news}
+      />
     </div>
   )
 }
